5c7d82903c35761bcf481064635891fb1c08e8a3
mynameis-sohee <eng.sohee@gmail.com>
2021-09-25 06:16:06 +0000

    table 중복 생성 및 오류 방지 쿼리 추가

--- airflow/dags/user_processing.py
+++ airflow/dags/user_processing.py
@@ -1,24 +1,19 @@
 from airflow.models import DAG
+# 첫번째 태스크
 from airflow.providers.sqlite.operators.sqlite import SqliteOperator
-from airflow.providers.http.sensors.http import HttpSensor
 from datetime import datetime
 
 # 파라미터 정의
-
 default_args = {
-    'start_date': datetime(2021, 8, 1)
+    'start_date': datetime(2020, 1, 1)
 }
 
-# start_date : 실제로 데이터 파이프라인이 예약되기 시작하는 날짜
-with DAG('user_processing', schedule_interval='@daily',
-default_args=default_args, catchup=False) as dag:
-    # task 및 operators 정의
-
+with DAG('user_processing', schedule_interval='@daily', default_args=default_args, catchup=False) as dag:
     creating_table = SqliteOperator(
         task_id='creating_table',
         sqlite_conn_id='db_sqlite',
         sql='''
-            CREATE TABLE users (
+            CREATE TABLE IF NOT EXISTS users (
                 firstname TEXT NOT NULL,
                 lastname TEXT NOT NULL,
                 country TEXT NOT NULL,
@@ -27,10 +22,4 @@ default_args=default_args, catchup=False) as dag:
                 email TEXT NOT NULL PRIMARY KEY
             );
         '''
-    )
-
-    is_api_available = HttpSensor(
-        task_id='is_api_available',
-        http_conn_id='user_api',
-        endpoint='api/'
     )
